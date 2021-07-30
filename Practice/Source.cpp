5c7d0ffd824321995eabc7da77de0ac092c86b42
ahavens1995 <87989306+ahavens1995@users.noreply.github.com>
2021-07-30 11:06:00 -0500

    cleaning up and fixing things

--- Practice/Source.cpp
+++ Practice/Source.cpp
@@ -11,53 +11,9 @@ using namespace std;
 #define PI 3.14159
 #define NEWLINE '\n'
 
-namespace first
-{
-	int x = 10;
-	int y = 11;
-}
-
-namespace second
-{
-	double x = 6.789;
-	double y = 7.192;
-}
-
-void dealWithMyAge(int age)
-{
-	if (age > 0)
-	{
-		if (age <= 50)
-		{
-			cout << "Damn " << age << " is young";
-			cout << NEWLINE;
-		}
-		else if (age > 50 && age <= 100)
-		{
-			cout << "Damn " << age << " is old";
-			cout << NEWLINE;
-		}
-		else
-		{
-			cout << "Your age, " << age << " is off the charts!";
-			cout << NEWLINE;
-		}
-
-	}
-
-	for (int c = 0; c < 100; c++)
-	{
-		age++;
-	}
-
-	cout << "Your new age is " << age;
-	cout << NEWLINE;
-}
-
-double calcCircleDiameter(double r)
-{
-	return 2 * PI * r;
-}
+string str = "supercalifragislisticipsyaladocious";
+string puppo = "dog";
+string notReal = "god";
 
 string reverseString(string s)
 {
@@ -85,30 +41,7 @@ void isPalindrome(string orig, string rev)
 	}
 }
 
-void countDown() 
-{
-	int countDown = 10;
-countItDown:
-	cout << countDown << ", ";
-	countDown--;
-	if (countDown > 0) goto countItDown;
-
-	cout << "Lift Off! ";
-	cout << NEWLINE;
-}
-
-void firstSecond()
-{
-	using first::x;
-	using second::y;
-
-	cout << x << "\n";
-	cout << y << "\n";
-	cout << first::y << "\n";
-	cout << second::x << "\n";
-}
-
-void mapTheString(string s)
+void matchingCharacters(string s)
 {
 
 	map<char, int> m;
@@ -130,38 +63,121 @@ void mapTheString(string s)
 	//print the map
 	for (it = m.begin(); it != m.end(); it++)
 	{
-		cout << it->first << " occurs " << it->second << " time(s)" << NEWLINE;
+		if (it->second != 1)
+		{
+			cout << it->first << " occurs " << it->second << " time(s)" << NEWLINE;
+		}
+		
 	}
 
 }
 
-
-//main function
-int main()
+void nonMatchingCharacters(string s)
 {
-	cout << "Hello World \n";
+
+	map<char, int> m;
+	map<char, int>::iterator it = m.begin();
+
+	for (int i = 0; i < int(s.length()); i++)
+	{
+		//if the character is not in the map
+		if (m.find(s.at(i)) == m.end())
+		{
+			m.insert(pair<char, int>(s.at(i), 1));
+		}
+		else
+		{
+			m.at(s.at(i)) += 1;
+		}
+	}
+
+	//print the map
+	for (it = m.begin(); it != m.end(); it++)
+	{
+		if (it->second == 1)
+		{
+			cout << it->first << " occurs " << it->second << " time(s)" << NEWLINE;
+		}
+
+	}
+
+}
+
+void calcVowelsConsonants(string s)
+{
+
+	int vow = 0;
+	int con = 0;
+	char c;
+
+	for (int i = 0; i < int(s.length()); i++)
+	{
+		c = s.at(i);
+
+		//if the character is not a val
+		if (c != 'a' && c != 'e' && c != 'i' && c != 'o' && c != 'u')
+		{
+			con += 1;
+		}
+		else
+		{
+			vow += 1;
+		}
+	}
+
+	cout << "There are " << vow << " vowel(s) in the string" << NEWLINE;
+	cout << "There are " << con << " consonant(s) in the string" << NEWLINE;
+
+}
+
+bool isAnagram(string s1, string s2)
+{
+	map<char, int> m1, m2;
 	
-	double circle = calcCircleDiameter(5.0);
+	for (int i = 0; i < int(s1.length()); i++)
+	{
+		//if the character is not in the map
+		if (m1.find(s1.at(i)) == m1.end())
+		{
+			m1.insert(pair<char, int>(s1.at(i), 1));
+		}
+		else
+		{
+			m1.at(s1.at(i)) += 1;
+		}
+	}
 
-	cout << circle;
-	cout << NEWLINE;
-	
-	int age;
-	//input age
-	cin >> age;
-	cout << NEWLINE;
-	//print your age
-	dealWithMyAge(age);
+	for (int h = 0; h < int(s2.length()); h++)
+	{
+		//if the character is not in the map
+		if (m2.find(s2.at(h)) == m2.end())
+		{
+			m2.insert(pair<char, int>(s2.at(h), 1));
+		}
+		else
+		{
+			m2.at(s2.at(h)) += 1;
+		}
+	}
 
-	countDown();
+	return m1.size() == m2.size() && equal(m1.begin(), m1.end(), m2.begin());
+}
 
-	firstSecond();
-
+void questionOne()
+{
+	cout << "How can you reverse a string?" << NEWLINE;
 	string orig = "cat";
 	string rev = reverseString(orig);
 
 	cout << rev;
-	cout << NEWLINE;
+	cout << NEWLINE << NEWLINE << NEWLINE;
+}
+
+void questionTwo()
+{
+	cout << "What is a palindrome string?" << NEWLINE;
+	string orig = "cat";
+	string rev = reverseString(orig);
 
 	isPalindrome(orig, rev);
 
@@ -174,9 +190,89 @@ int main()
 
 	isPalindrome(orig, rev);
 
-	orig = "supercalifragislisticipsyaladocious";
+	cout << NEWLINE << NEWLINE << NEWLINE;
+}
 
-	mapTheString(orig);
+void questionThree()
+{
+	cout << "How to get the matching characters in a string?" << NEWLINE;
+	matchingCharacters(str);
+	cout << NEWLINE << NEWLINE << NEWLINE;
+}
+
+void questionFour()
+{
+	cout << "How to get the non-matching characters in a string?" << NEWLINE;
+	nonMatchingCharacters(str);
+	cout << NEWLINE << NEWLINE << NEWLINE;
+}
+
+void questionFive()
+{
+	cout << "How to calculate the number of vowels and consonants in a string?" << NEWLINE;
+	calcVowelsConsonants(str);
+	cout << NEWLINE << NEWLINE << NEWLINE;
+}
+
+void questionSix()
+{
+	cout << "How do you prove that the two strings are anagrams?" << NEWLINE;
+	bool isTrue = isAnagram(puppo, notReal);
+	
+	if (isTrue)
+	{
+		cout << puppo << " and " << notReal << " are anagrams" << NEWLINE;
+	}
+	else
+	{
+		cout << puppo << " and " << notReal << " are not anagrams" << NEWLINE;
+	}
+
+	isTrue = isAnagram(puppo, str);
+
+	if (isTrue)
+	{
+		cout << puppo << " and " << str << " are anagrams" << NEWLINE;
+	}
+	else
+	{
+		cout << puppo << " and " << str << " are not anagrams" << NEWLINE;
+	}
+
+	cout << NEWLINE << NEWLINE << NEWLINE;
+}
+
+void questionSeven()
+{
+	cout << "Find the count for the occurrence of a particular character in a string." << NEWLINE;
+
+}
+
+//main function
+int main()
+{
+	cout << "Hello World \n" << NEWLINE << NEWLINE << NEWLINE;
+	
+	//Q1
+	questionOne();
+
+	//Q2
+	questionTwo();
+
+	//Q3
+	questionThree();
+
+	//Q4
+	questionFour();
+
+	//Q5
+	questionFive();
+
+	//Q6
+	questionSix();
+
+	//Q7
+	questionSeven();
 
 	return 0;
 
