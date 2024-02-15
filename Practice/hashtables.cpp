891dfc6a24fdef45597aa84ef21a116d8f69a54c
Aurora Havens <87989306+ahavens1995@users.noreply.github.com>
2024-02-15 12:48:23 -0600

    created hashtables.cpp

---
+++ Practice/hashtables.cpp
@@ -0,0 +1,14 @@
+//input output
+#include <iostream>
+//string functions
+#include <string>
+//include mapping data structure
+#include <map>
+//is digit, is char
+#include<stdio.h>
+#include<ctype.h>
+//array
+#include <array>
+
+//shortening
+using namespace std;
