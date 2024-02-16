42996e9528b11b2253b92ea00ca81d0c7f07b169
Aurora Havens <87989306+ahavens1995@users.noreply.github.com>
2024-02-16 12:58:34 -0600

    added to hashtable.cpp

--- Practice/hashtables.cpp
+++ Practice/hashtables.cpp
@@ -1,3 +1,6 @@
+//Code taken from https://www.digitalocean.com/community/tutorials/hash-table-in-c-plus-plus
+//Used for educational/tutorial purposes
+
 //input output
 #include <iostream>
 //string functions
@@ -12,3 +15,105 @@
 
 //shortening
 using namespace std;
+
+define CAPACITY 50000 //size of hashtable
+
+unsigned long hash_function(char* str)
+{
+    unsigned long i = 0;
+
+    for (int j = 0; str[j]; j++)
+        i += str[j];
+    
+    return i % CAPACITY;
+}
+
+typedef struct Hash_Item
+{
+
+    char* key;
+    char* val;
+
+}Hash_Item;
+
+typedef struct HashTable
+{
+
+    Hash_Item** items;
+    int size;
+    int count;
+
+}HashTable;
+
+Hash_Item* create_item(char* key, char* val)
+{
+
+    Hash_Item* item = (Hash_Item*) malloc(sizeof(Hash_Item));
+    item->key = (char*) malloc(strlen(key) + 1);
+    item->val = (char*) malloc(strlen(val));
+    
+    strcpy(item->key, key);
+    strcpy(item->val, val);
+    
+    return item;
+
+}
+
+HashTable* create_table(int size)
+{
+    //Create a new Hashtable
+    HashTable* table = (HashTable*) malloc(sizeof(HashTable));
+    table->size = size;
+    table->count = 0;
+    table->items = (Hash_Item**) calloc(table->size, sizeof(Hash_Item*));
+
+    for(int i = 0; i < table->size; i++)
+    {
+        table->items[i] = NULL;
+    }
+
+    return table;
+}
+
+void free_item(Hash_Item* item)
+{
+    free(item->key);
+    free(item->value);
+    free(item);
+}
+
+void free_Table(HashTable* table)
+{
+    //free the table
+    for(int i = 0; i < table->size; i++)
+    {
+        Hash_Item* item = table->items[i];
+
+        if(item != NULL)
+        {
+            free_item(item);
+        }
+    }
+
+    free(table->items);
+    free(table);
+}
+
+void print_table(HashTable* table)
+{
+    printf("\nHash Table\n--------------------\n");
+
+    for(int i = 0; i < table->size; i++)
+    {
+        if(table->items[i])
+        {
+            printf("Index:%d, Key:%s, Value:%s\n", i, table->items[i] -> key, table->items[i]->value);
+        }
+    }
+
+    printf("----------------\n\n");
+}
+
+int main(){
+
+};
